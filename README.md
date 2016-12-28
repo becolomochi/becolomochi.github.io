8552caa7d71dc8b5bedd145ded7ad16d8bc09213
becolomochi <becolomochi@gmail.com>
2016-12-28 23:40:00 +0900

    add contents

---
+++ README.md
@@ -0,0 +1,2 @@
+becolomochi.github.io
+======================
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+var cssmin = require('gulp-cssmin');
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch("./less/**/*.less", ['less']);
+  gulp.watch("./*.html", ['bs-reload']);
+  gulp.watch("./css/**/*.css", ['bs-reload']);
+});
+gulp.task('browser-sync', function() {
+  browserSync.init({
+	server: {
+	  index: "index.html"
+	}
+  });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+	.pipe(cssmin())
+    .pipe(gulp.dest('./css'));
+});
